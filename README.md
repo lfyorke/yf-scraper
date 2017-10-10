252ad65a8a449541cef8e476cef7d04ba18ecda4
Leo Yorke <Leo.Yorke@keyruscorp.key>
2017-10-10 10:22:57 +0100

    First commit of initial base webscraper

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # yf-scraper
 Yahoo Finance currency exchange rates webscraper
+
+## About
+This script will go to Yahoo Finance's [currency website](https://uk.finance.yahoo.com/currencies) and fetch the current list of fx pairs and their assosciated rates.
---
+++ currency-webscraper.py
@@ -0,0 +1,24 @@
+import bs4
+import pandas as pd 
+import requests
+import urllib.request
+
+URL = 'https://uk.finance.yahoo.com/currencies'
+COLUMNS = ['cy-pair', 'rate']
+
+def get_webpage(url):
+    response = requests.get(url)
+    return bs4.BeautifulSoup(response.text, 'html.parser')
+
+def scrape(webpage):
+    rows = webpage.find("table").find_all("tr")
+    cy_data = []
+    for row in rows:
+        cells = row.find_all("td")[1:3]
+        cy_data.append([cell.text for cell in cells])
+    return pd.DataFrame(cy_data, columns=COLUMNS).drop(0, axis=0)
+
+if __name__ == "__main__":
+    page = get_webpage(URL)
+    data = scrape(page)
+    print(data)
